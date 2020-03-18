7e287b3a3e146a391adbdbbda910ca348071f3b1
Luís Macedo <luisjrsm@hotmail.com>
2020-03-18 20:59:27 +0000

    updates

--- guiao3/src/main/java/Step3.java
+++ guiao3/src/main/java/Step3.java
@@ -16,8 +16,7 @@ public class Step3 {
         Admin admin = conn.getAdmin();
         HTableDescriptor t = new HTableDescriptor(TableName.valueOf("actor"));
         t.addFamily(new HColumnDescriptor("name"));
-        t.addFamily(new HColumnDescriptor("birth"));
-        t.addFamily(new HColumnDescriptor("death"));
+        t.addFamily(new HColumnDescriptor("dates"));
         t.addFamily(new HColumnDescriptor("nmovies"));
         t.addFamily(new HColumnDescriptor("top3"));
         t.addFamily(new HColumnDescriptor("collab"));
@@ -26,12 +25,30 @@ public class Step3 {
 
         Table ht = conn.getTable(TableName.valueOf("actor"));
         for(int i=0; i<10; i++) {
+            String collaborators;
+            if(i==0){
+                collaborators = "&A" + (i + 1);
+            }
+            else{
+                if(i==9){
+                    collaborators = "&A" + (i - 1);
+                }
+                else {
+                    collaborators = "&A" + (i - 1) + ";A" + (i + 1);
+                }
+            }
+
             byte[] rowKey = Bytes.add(Bytes.toBytes("A" + i), Bytes.toBytes("&201" + i + "-" + i + "-1"), Bytes.toBytes("&201" + (i + 30) + "-" + i + "-1"),
-                    Bytes.toBytes("&" + (14 + i)), Bytes.toBytes("&Movie" + i + ";Movie" + (i+1)), Bytes.toBytes("&"));
+                    Bytes.toBytes("&" + (14 + i)), Bytes.toBytes("&FILME" + i + ";FILME" + (i+1)), Bytes.toBytes(collaborators));
+
             Put put = new Put(rowKey);
 
-            // Nao percebo a seguinte linha
-            //put.addColumn(Bytes.toBytes("qual"), Bytes.toBytes("q"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("name"), Bytes.toBytes("n"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("dates"), Bytes.toBytes("b"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("dates"), Bytes.toBytes("d"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("nmovies"), Bytes.toBytes("nm"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("top3"), Bytes.toBytes("t"), Bytes.toBytes(0L));
+            put.addColumn(Bytes.toBytes("collab"), Bytes.toBytes("c"), Bytes.toBytes(0L));
 
             ht.put(put);
         }
