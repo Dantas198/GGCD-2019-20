b4fb48ccb76554d991556f61b79ee6c07b015998
GuilhermeViveiros <a80524@alunos.uminho.pt>
2020-03-27 17:54:38 +0000

    Guiao 5, falta dar o sort

--- Large Scale Data Management Spark/src/main/java/Spark.java
+++ Large Scale Data Management Spark/src/main/java/Spark.java
@@ -15,21 +15,6 @@ public class Spark {
 
         double begin = System.nanoTime();
 
-        JavaPairRDD<String, Integer> mr = sc.textFile("/Users/guilhermeviveiros/Desktop/4ano/4ano-2semestre/GGCD/Large Scale Data Management Spark/title.basics.tsv.bz2")
-                .map(l -> l.split("\t"))
-                .filter(l -> !l[0].equals("tconst"))
-                .map(l -> l[8])
-                .filter(l -> !l.equals("\\N"))
-                .flatMap(l -> Arrays.asList(l.split(",")).iterator())
-                .mapToPair(l -> new Tuple2<>(l, 1))
-                .foldByKey(0, (v1, v2) -> v1 + v2);
-
-        //List<Tuple2<String, Integer>> genres = mr.collect();
-
-        double end = System.nanoTime();
-
-        //System.out.println("Time in the mini file = " + (begin-end));
-
         // Compute a list showing the rating for each title using also title.ratings.tsv.gz
 
         JavaPairRDD<String, Float> mr2 = sc.textFile("/Users/guilhermeviveiros/Desktop/4ano/4ano-2semestre/GGCD/Large Scale Data Management Spark/title.ratings.tsv.bz2")
@@ -37,24 +22,14 @@ public class Spark {
                 .filter(l -> !l[0].equals("tconst"))
                 .filter(l -> !l.equals("\\N"))
                 .mapToPair(l -> new Tuple2<>(l[0], Float.parseFloat(l[1])))
+
+                //Compute a list of films with rating of at least 9.0, sorted by rating.
                 .filter( l ->  l._2 > 9.0);
 
 
+        double end = System.nanoTime();
 
-
-        //.filter( (fst,snd) -> fst.toString().equals("ola"));
-
-
-
-
-        //.foldByKey(0, (v1, v2) -> v1 + v2);
-        //Compute a list of films with rating of at least 9.0, sorted by rating.
-
-
-                //.filter(l -> !l.equals("\\N"))
-                //.flatMap(l -> Arrays.asList(l.split(",")).iterator())
-                //.mapToPair(l -> new Tuple2<>(l, 1))
-                //foldByKey(0, (v1, v2) -> v1 + v2);
+        //System.out.println("Time in the mini file = " + (begin-end));
 
         List<Tuple2<String, Float>> votes = mr2.collect();
 
